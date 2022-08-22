503580eaf909155236b081c81f842ad7669465cd
廖健傑 <a36118@adcmuh.org.tw>
2022-08-22 13:05:00 +0800

    add chapter9

---
+++ 6 小時快速上手 Angular + TypeScript/chapter9/src/app/app.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { HttpClientModule } from '@angular/common/http';
+import { ReactiveFormsModule } from '@angular/forms';
+import { GitHubService } from './github.service';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    HttpClientModule,
+    ReactiveFormsModule
+  ],
+  providers: [GitHubService],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
